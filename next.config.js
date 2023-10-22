3a8a635ec3ae402de9342aca1422d5530c57efd8
shrenikpatel17 <shrenik.d.patel@gmail.com>
2023-10-22 04:21:35 -0400

    bugfix

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
     experimental: {
         serverComponentsExternalPackages: ['pdf2json'],
     },
-    future: { webpack5: true },
-    webpack: (config, {}) => {
-        config.resolve.alias.canvas = false
-        config.resolve.alias.encoding = false
-        return config
-    }
 };
 
 module.exports = nextConfig
