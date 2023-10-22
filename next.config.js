ebc9da0f64583ed32b4203fcced4678f11a5b46f
shrenikpatel17 <shrenik.d.patel@gmail.com>
2023-10-22 01:34:56 -0400

    Fixed styling issues

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
     experimental: {
         serverComponentsExternalPackages: ['pdf2json'],
     },
+    future: { webpack5: true },
+    webpack: (config, {}) => {
+        config.resolve.alias.canvas = false
+        config.resolve.alias.encoding = false
+        return config
+    }
 };
 
 module.exports = nextConfig
