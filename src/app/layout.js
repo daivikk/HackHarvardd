db4bb896399d33170b0301b33d9a3baf5e5001db
daivikk <daivik.d.patel@gmail.com>
2023-10-22 03:55:38 -0400

    Added Favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from '@/state/provider'
+import Head from './head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head />
       <body className={inter.className}>
         <Providers>
           {children}
